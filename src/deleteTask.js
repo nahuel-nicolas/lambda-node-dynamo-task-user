558d67c5c225b122f68422a2edb2609ed43893dc
nahuel-nicolas <nnahuel012@gmail.com>
2023-04-18 15:14:12 -0300

    serverless.functions.deleteTask: Created deleteTask function

---
+++ src/deleteTask.js
@@ -0,0 +1,23 @@
+const AWS = require("aws-sdk");
+
+
+module.exports.deleteTask = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const { id } = event.pathParameters;
+
+  await dynamodb
+    .delete({
+      TableName: "TaskTable",
+      Key: {
+        id,
+      },
+    })
+    .promise();
+
+  return {
+    status: 200,
+    body: {
+      message: 'Deleted Task'
+    }
+  };
+};
